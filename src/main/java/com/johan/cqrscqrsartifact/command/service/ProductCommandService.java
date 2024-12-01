fc660cefa9696a94cc913320acec23cdcbc279a2
Your Name <you@example.com>
2024-11-30 23:22:24 -0500

    all classe

--- src/main/java/com/johan/cqrscqrsartifact/command/service/ProductCommandService.java
+++ src/main/java/com/johan/cqrscqrsartifact/command/service/ProductCommandService.java
@@ -1,21 +1,28 @@
 package com.johan.cqrscqrsartifact.command.service;
 
 import com.johan.cqrscqrsartifact.command.entity.Product;
+import com.johan.cqrscqrsartifact.command.event.ProductCreatedEvent;
 import com.johan.cqrscqrsartifact.command.repository.ProductRepository;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductCommandService {
     private final ProductRepository productRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public ProductCommandService(ProductRepository productRepository) {
+    public ProductCommandService(ProductRepository productRepository, ApplicationEventPublisher applicationEventPublisher) {
         this.productRepository = productRepository;
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 
     public Product createProduct(String name, Double price) {
         Product product = new Product();
         product.setName(name);
         product.setPrice(price);
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(this, product.getId(), product.getName(), product.getPrice());
+        applicationEventPublisher.publishEvent(productCreatedEvent);
         return productRepository.save(product);
+
     }
 }
